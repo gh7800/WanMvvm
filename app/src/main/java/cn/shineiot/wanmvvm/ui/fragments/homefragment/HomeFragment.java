936b51f12c5785e3efb431a8403449a16f45c2ed
wangshuai <bjwangs@163.com>
2020-06-12 17:44:58 +0800

    ..

--- app/src/main/java/cn/shineiot/wanmvvm/ui/fragments/homefragment/HomeFragment.java
+++ app/src/main/java/cn/shineiot/wanmvvm/ui/fragments/homefragment/HomeFragment.java
@@ -2,14 +2,18 @@ package cn.shineiot.wanmvvm.ui.fragments.homefragment;
 
 import android.view.View;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.youth.banner.Banner;
+
+import java.util.List;
 
 import butterknife.BindView;
 import cn.shineiot.base.base.BaseFragment;
+import cn.shineiot.base.utils.LogUtil;
 import cn.shineiot.wanmvvm.R;
+import cn.shineiot.wanmvvm.bean.AndroidArticle;
 
 /**
  * @author GF63
@@ -29,8 +33,19 @@ public class HomeFragment extends BaseFragment<HomeViewModel> {
 
 	@Override
 	protected void initView(View mFragmentView) {
-		viewModel.getBanner();
-		viewModel.getLiveDataArticle(page);
+		viewModel.getBanner().observeForever(new Observer<List<cn.shineiot.wanmvvm.bean.Banner>>() {
+			@Override
+			public void onChanged(List<cn.shineiot.wanmvvm.bean.Banner> banners) {
+				LogUtil.e(banners.get(0).getTitle());
+			}
+		});
+
+		/*viewModel.getLiveDataArticle(page).observeForever(new Observer<List<AndroidArticle>>() {//参数不对
+			@Override
+			public void onChanged(List<AndroidArticle> androidArticles) {
+				LogUtil.e(androidArticles.size());
+			}
+		});*/
 	}
 
 	@Override
